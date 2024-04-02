f00ae3c73018a24265dfb501d0cb2291efe06373
Wes Hays <weshays@gmail.com>
2024-04-02 11:52:36 -0700

    Added CRUD methods

--- temperature/urls.py
+++ temperature/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<id>", views.show, name='show'),
+    path("new", views.new, name="new"),
+    path("create", views.create, name="create"),
+    path("edit/<id>", views.edit, name="edit"),
+    path("update/<id>", views.update, name="update"),
+    path("delete/<id>", views.delete, name="delete")
 ]
--- temperature/views.py
+++ temperature/views.py
@@ -10,7 +10,7 @@ def getIPData():
         ip = requests.get("https://api.ipify.org/?format=text").text
         re = requests.get(f"https://ipapi.co/{ip}/json/", headers={"user-agent":f"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/{random.randint(100,999)}.36"}).json()
         #return [str(re),ip,"32.6917","-117.1151"]
-        return [re['city'],re["region_code"],re['latitude'],re['longitude']]   
+        return [re['city'],re["region_code"],re['latitude'],re['longitude']]
     except Exception as e:
         return [e,"NUL","32.6917","-117.1151"]
 def fetch_temperature_data(request = "172.12.246.61"):
@@ -29,5 +29,32 @@ def fetch_temperature_data(request = "172.12.246.61"):
 # Create your views here.
 def index(request):
     context = {"fetch_temperature_data":fetch_temperature_data(request)}
-    return render(request, 'temperature_structure.html',context=context)
+    return render(request, 'index.html',context=context)
+
+def show(request, id):
+    context = {"id": id}
+    return render(request, 'show.html',context=context)
+
+def new(request):
+    return render(request, 'new.html')
+
+def create(request): # Post request
+    # redirect to index
+    return render(request, 'index.html')
+
+def edit(request, id): # Put request
+    context = {"id": id}
+    return render(request, 'edit.html',context=context)
+
+def update(request, id):
+    context = {"id": id}
+    # return render(request, 'update.html',context=context)
+    # Redirect to index in python django
+    return render(request, 'index.html')
+
+def delete(request, id): # Delete request
+    context = {"id": id}
+    # return render(request, 'delete.html',context=context)
+    # Redirect to index in python django
+    return render(request, 'index.html')
 
